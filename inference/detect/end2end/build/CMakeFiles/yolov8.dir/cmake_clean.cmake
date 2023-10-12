e096cef474378fdbb7824d6525d6fcd46b9e1eb8
lin <904798869@qq.com>
2023-10-12 16:57:19 +0800

    update

---
+++ inference/detect/end2end/build/CMakeFiles/yolov8.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/yolov8.dir/main.cpp.o"
+  "yolov8"
+  "yolov8.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/yolov8.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
